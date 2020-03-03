b6b72470a96c22aa54bca21321a65f264c41e1f5
liuzhao2580 <1457639270@qq.com>
2020-03-03 17:46:52 +0800

    更多歌单页面的样式完成，tab栏的效果有待处理

--- miniprogram/components/com-list/com-list.js
+++ miniprogram/components/com-list/com-list.js
@@ -1,4 +1,4 @@
-// pages/tabbar/home/components/com-recommand/com-recommand.js
+// components/com-list/com-list.js
 Component({
     /**
      * 组件的属性列表
--- miniprogram/pages/songSheet/songSheet.js
+++ miniprogram/pages/songSheet/songSheet.js
@@ -1,66 +1,49 @@
 // pages/songSheet/songSheet.js
+const db = wx.cloud.database()
 Page({
-
-    /**
-     * 页面的初始数据
-     */
     data: {
-
+        recommandData: [],
+        head_title: [
+            {
+                label: "推荐"
+            },
+            {
+                label: "官方"
+            },
+            {
+                label: "精品"
+            },
+            {
+                label: "华语"
+            },
+            {
+                label: "欧美"
+            },
+            {
+                label: "日韩"
+            },
+            {
+                label: "摇滚"
+            },
+            {
+                label: "民谣"
+            }
+        ],
+        isActive: 0
     },
-
-    /**
-     * 生命周期函数--监听页面加载
-     */
     onLoad: function (options) {
-
+        // 2. 热门推荐的数据
+        db.collection("album").get().then(({ data }) => {
+            this.setData({
+                recommandData: data
+            })
+        })
     },
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
+    itemTap(event) {
+        const { currentTarget } = event
+        const currentIndex = currentTarget.dataset.currentindex
+        this.setData({
+            isActive: currentIndex
+        })
     }
 })
