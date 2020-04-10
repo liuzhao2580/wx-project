c55d47cde72648f85938a94d81f3887da9f1a0af
liuzhao2580 <1457639270@qq.com>
2020-04-10 09:33:56 +0800

    个人中心

--- miniprogram/pages/tabbar/personal/personal.js
+++ miniprogram/pages/tabbar/personal/personal.js
@@ -5,12 +5,57 @@ Page({
      * 页面的初始数据
      */
     data: {
+        operationArray: [
+            {
+                title: "我的订单",
+                array: [{
+                        src: "",
+                        title: "待付款"
+                    },
+                    {
+                        src: "",
+                        title: "待收货"
+                    }, {
+                        src: "",
+                        title: "已完成"
+                    }, {
+                        src: "",
+                        title: "全部"
+                    }
+                ]
+            },
+            {
+                title: "我的工具",
+                array: [
+                    {
+                        src: "",
+                        title: "收货地址"
+                    },
+                    {
+                        src: "",
+                        title: "支付密码"
+                    },
+                    {
+                        src: "",
+                        title: "我的拼团"
+                    },
+                    {
+                        src: "",
+                        title: "官方客服"
+                    },
+                    {
+                        src: "",
+                        title: "发票服务"
+                    }
+                ]
+            }
+        ]
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad: function (options) {
-        
+    onLoad: function(options) {
+
     },
 })
